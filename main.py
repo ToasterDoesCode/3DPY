d82fd454d62747a754763623b39e0d9a1ea4b2ee
Octaconta <47088549+ToasterDoesCode@users.noreply.github.com>
2023-12-18 14:02:03 -0500

    Add files via upload
    Not working but progress nonetheless.

--- main.py
+++ main.py
@@ -1,205 +1,183 @@
 # Imports
-from PIL import Image, ImageDraw, ImageOps
-from msvcrt import getch
 import math
 import pygame
+import numpy as np
+import threading
 
-# Get OBJ path from user
-path = input("Please enter the path to the OBJ file you'd like to render: \n")
-print()
+# Needed global variables
+# Display
+screen_size = screen_width, screen_height = 1080, 720
 
-# Declare and initialize needed global variables
-vertices_obj = []
+display_foreground = (255, 255, 255)
+display_background = (0, 0, 0)
 
-faces_obj = []
+display_grid = (120, 120, 120)
 
-offset = 0.01
+scale = 10
 
-# Camera properties
-focal_length = 0.005
-
+# Camera
+fov = 95
 cam_pos = [0, 0, 0]
+cam_rot = [0, 0, 0]
+cam_plane = [0, 0, 5]
 
-# Control properties
-mouse_sens = 0.1
+# OBJ data
+obj_faces = []
+obj_vertices = []
 
-# Open OBJ
-with open(path, "r") as f:
-    # Get information from OBJ file
-    data = f.readlines()
-    
-    # Get vertices from OBJ file
-    for line in data:
-        if line[0] == "v" and line[1] == " ":
-            # line is a vertex
-            
-            vertex = []
-            
-            i = 0
-            
-            for substring in line.split(" ")[1:]:
-                vertex.append(float(substring) + offset + cam_pos[i])
-                i += 1
-            
-            vertices_obj.append(vertex)
-            
-    
-    # Get faces from OBJ file
-    for line in data:
+# Game control
+game_running = False
 
-        if line[0] == "f":
-            # line is a face
-            face = []
-            
-            for substring in line.split(" ")[1:]:
-                face.append(vertices_obj[int(substring.split("/")[0]) - 1])
-            faces_obj.append(face)
-
-scale = int(input("What scale?"))
+# Necessary functions
+def transform_vector(v, m):
+    return np.array(v) @ np.array(m)
 
 # Rotate vertex
-def rotate_vertex(vert, axis, deg):
-    rot_x = [
+def rotate_project_vertex(vert, deg, origin):
+    x_rot = [
         [1, 0, 0],
-        [0, math.cos(deg[0]), -math.sin(deg[0])],
-        [0, math.sin(deg[0]), math.cos(deg[0])]
+        [0, math.cos(deg[0]), math.sin(deg[0])],
+        [0, -math.sin(deg[0]), math.cos(deg[0])]
     ]
     
-    rot_y = [
-        [math.cos(deg[1]), 0, math.sin(deg[1])],
+    y_rot = [
+        [math.cos(deg[1]), 0, -math.sin(deg[1])],
         [0, 1, 0],
-        [-math.sin(deg[1]), 0, math.cos(deg[1])]
+        [math.sin(deg[1]), 0, math.cos(deg[1])]
     ]
     
-    rot_z = [
-        [math.cos(deg[2]), -math.sin(deg[2]), 0],
-        [math.sin(deg[2]), math.cos(deg[2]), 0],
+    z_rot = [
+        [math.cos(deg[2]), math.sin(deg[2]), 0],
+        [-math.sin(deg[2]), math.cos(deg[2]), 0],
         [0, 0, 1]
     ]
     
-    rot = []
+    # Combine the matrices by multiplying them together
+    rot = np.array(x_rot) @ np.array(y_rot) @ np.array(z_rot)
     
-    new_coordinates = [0, 0, 0]
-    for a in range(len(axis)):
+    # Project
+    projected_vector = np.subtract(np.array(vert), np.array(origin)) @ rot
     
-        if axis[a] == "x":
-            rot = rot_x
-        elif axis[a] == "y":
-            rot = rot_y
-        elif axis[a] == "z":
-            rot = rot_z
+    # Transform to screen coordinates and return
+    return [(((cam_plane[2] / projected_vector[2]) * projected_vector[0] + cam_plane[0]) * scale) + screen_width / 2, (((cam_plane[2] / projected_vector[2]) * projected_vector[1] + cam_plane[1]) * scale) + screen_height / 2]
+
+# Read an OBJ
+def read_obj(path):
+    with open(path, 'r') as f:
+        # Read lines of file
+        obj_lines = f.readlines()
         
-        for i in range(2):
-            new_coordinates[i] = rot[i][0] * vert[0] + rot[i][1] * vert[1] + rot[i][2] * vert[2]
+        # Loop through each line and read
+        for obj_line in obj_lines:
+            # Check if line is a vertex
+            if obj_line[0] == "v" and obj_line[1] == " ":
+                # Line is a vertex, read
+                # Temporary variable to store current vertex
+                obj_current_vertex = []
+                
+                for obj_coord in obj_line.split(" ")[1:]:
+                    obj_current_vertex.append(float(obj_coord))
+                
+                # Add to all vertices
+                obj_vertices.append(obj_current_vertex)
+                
+            # Check if line is a face
+            if obj_line[0] == "f":
+                # Line is a face, read
+                # Temporary variable to store current face
+                obj_current_face = []
+                
+                for obj_vertex_i in obj_line.split(" ")[1:]:
+                    # Add vertex at index to face
+                    obj_current_face.append(obj_vertices[int(obj_vertex_i.split("/")[0]) - 1])
+                
+                # Add to all faces
+                obj_faces.append(obj_current_face)
+                
+def draw_frame(faces):
+    # Out variable to store output
+    projected_vertices = []
     
-    return new_coordinates
-
-# Rotate object
-def rotate_object(axis, deg):
-    faces_rot = []
-
-    for f in range(len(faces_obj)):
-        for v in range(len(faces_obj[f])):
-            face_rot.append(rotate_vertex(faces_obj[f][v], axis, deg))
-        faces_rot.append(face_rot)
-    return faces_rot
-
-# Draw a frame
-def draw_frame(faces, rotation=(0, 0, 0)):
-    img = Image.new("RGB", (1920, 1080))
-    draw = ImageDraw.Draw(img)
-    
-    projected_faces = []
-    
+    # Loop through each face
     for face in faces:
-        face_projected = []
-        
+        current_projected_face = []
+    
+        # Loop through each vertex
         for vertex in face:
+            # Project and store
+            projected_vertex = rotate_project_vertex(vertex, cam_rot, cam_pos)
+            projected_vertices.append(projected_vertex)
             
-            x = rotate_vertex(vertex, "xy", rotation)[0] + offset - cam_pos[0]
-            y = rotate_vertex(vertex, "yz", rotation)[1] + offset - cam_pos[1]
-            z = rotate_vertex(vertex, "xz", rotation)[2] + offset - cam_pos[2]
-
-            vertex_projected_screen = ((((x / (focal_length /z))) * scale) + 1920 / 2, ((y / (focal_length / z)) * scale + 1080 / 2))
             
-            face_projected.append(vertex_projected_screen)
-            
-        projected_faces.append(face_projected)
         
-        
-    # Get face averages and depths
-    for i in range(len(faces)):
-        face_average = []
-        
-        for v in range(len(faces[i])):
-            face_average.append(faces[i][v][0])
-        
-        first = sum(face_average) / len(face_average)
-        face_average = []
-        
-        for v in range(len(faces[i])):
-            face_average.append(faces[i][v][1])
-        
-        second = sum(face_average) / len(face_average)
-        face_average = []
-        
-        for v in range(len(faces[i])):
-            face_average.append(faces[i][v][2])
-        
-        third = sum(face_average) / len(face_average)
-        face_average = (float(first), float(second), float(third))
-        
-        depth = math.sqrt(face_average[0] ** 2 + face_average[1] ** 2 + face_average[2] ** 2)
-        projected_faces[i].append(depth)
-    
-    projected_faces = sorted(projected_faces, key=lambda x:x[len(x) - 1], reverse=True)
-    
-    # Prep faces array
-    for i in range(len(projected_faces)):
-        projected_faces[i].pop(len(projected_faces[i]) - 1)
-    
-    # Render lines
-    for face in projected_faces:
-        
-        for i in range(1, len(face)):
-            pygame.draw.line(screen, "white", (face[i - 1][0], face[i - 1][1]), (face[i][0], face[i][1]))
-    
-    draw_points = True
-    point_radius = 2
-    
-    # Render points
-    if draw_points:
-        for face in projected_faces:
-            for vertex_projected in face:
-                pygame.draw.circle(screen, "white", vertex_projected, 5)
-    
-    img = ImageOps.flip(img)
-    return img
+        # Draw face
+        #pygame.draw.polygon(screen, display_foreground, current_projected_face)
 
-current_faces = faces_obj
-
-# pygame setup
+    vertex_depths = []
+    
+    # Draw
+    for face in faces:
+        for vertex in face:
+            # Get vertex depth
+            vertex_depths.append(math.sqrt((vertex[0] - cam_pos[0]) ** 2) + (vertex[1] - cam_pos[1]) ** 2 + (vertex[2] - cam_pos[2]) ** 2)
+    
+    for vertex in sorted(projected_vertices, key=lambda x : vertex_depths[projected_vertices.index(x)]):
+        pygame.draw.circle(screen, display_foreground, vertex, 5)
+    
+# Set up pygame
 pygame.init()
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
+
+# Obj stuff
+obj_file = input()
+read_obj(obj_file)
+
 running = True
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # Key presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                cam_pos[0] -= math.sin(cam_rot[0]) * math.cos(cam_rot[1])
+                cam_pos[1] -= math.sin(cam_rot[0]) * math.sin(cam_rot[1])
+                cam_pos[2] -= math.cos(cam_rot[0])
+            if event.key == pygame.K_s:
+                cam_pos[0] += math.sin(cam_rot[0]) * math.cos(cam_rot[1])
+                cam_pos[1] += math.sin(cam_rot[0]) * math.sin(cam_rot[1])
+                cam_pos[2] += math.cos(cam_rot[0])
+            if event.key == pygame.K_a:
+                cam_pos[1] -= math.sin(cam_rot[0]) * math.cos(cam_rot[1])
+                cam_pos[0] -= math.sin(cam_rot[0]) * math.sin(cam_rot[1])
+                cam_pos[2] -= math.cos(cam_rot[0])
+            
+            if event.key == pygame.K_UP:
+                cam_rot[0] -= 5
+            if event.key == pygame.K_DOWN:
+                cam_rot[0] += 5
+            if event.key == pygame.K_LEFT:
+                cam_rot[1] -= 5
+            if event.key == pygame.K_RIGHT:
+                cam_rot[1] += 5
+                
+        if event.type == pygame.MOUSEWHEEL:
+            cam_plane[2] += event.y
+    
+    # PROBLEM: Roll (Z component of cam_rot) is unknown, need to calculate it somehow.
+    cam_rot[0] = math.radians(pygame.mouse.get_pos()[0] / 2)
+    cam_rot[1] = math.radians(-pygame.mouse.get_pos()[1] / 2)
+    
+    # Refresh screen
+    screen.fill(display_background)
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+    draw_frame(obj_faces)
 
-    # Draw a frame
-    draw_frame(current_faces, rotation=(0, pygame.mouse.get_pos()[0] * -mouse_sens, pygame.mouse.get_pos()[1] * mouse_sens))
-
-    # flip() the display to put your work on screen
+    # Draw to screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    # Limit FPS
+    clock.tick(60)
 
 pygame.quit()
